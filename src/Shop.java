87455a56934c16629714b4d3b00042328f752886
Clarissa Mercado <clarissam5>
2018-08-20 10:28:51 -0400

    Quiz 7 Updates

--- src/Shop.java
+++ src/Shop.java
@@ -7,6 +7,18 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Shop {
+	
+	public static boolean isShopOpen = true;
+	public static double totalAmount = 0.0; 
+	public static int userInput = 9; 
+	public static Product[] inventory = { new Product("Business Ritual Women's Long Sleeve Top", 65.39, "Apparel", 4, 'L', "Blue") , 
+				new Product("The Art of Computer Programming", 190.54, "Books", 9, "Donald E. Knuth"),
+				new Product("Men's Business Cascual Shirt", 57.10, "Apparel", 2, 'M', "Teal"),
+				new Product ("Organic Tumeric Soap", 11.25, "Bath & Beauty", 1),
+				new Product ("Head First Design Pattern", 37.35, "Books", 3, "Eric Freeman"),
+		};
+			
+	
 	public static void main (String args[]) {
 		/*//Creating Product objects
 		Product prod1 = new Product("Business Ritual Women's Long Sleeve Top", 65.39, "Apparel", 4, 'L', "Blue");
@@ -15,23 +27,29 @@ public class Shop {
 		Product prod4 = new Product("Organic Tumeric Soap", 1.25, "Bath & Beauty", 1);
 		Product prod5 = new Product("Head First Design Pattern", 37.35, "Books", 3, "Eric Freeman");
 		*/
+		//Checking if the shop is not open 
+		if (!isShopOpen) {
+			System.out.println("The Shop is not open.");
+		}else {
+		
 		//Array of Product objects called inventory
-		Product[] inventory = new Product [5]; //how many elements in student array 
+		/*Product[] inventory = new Product [5]; //how many elements in student array 
 		  	inventory[0]= new Product("Business Ritual Women's Long Sleeve Top", 65.39, "Apparel", 4, 'L', "Blue");
 		  	inventory[1]= new Product("The Art of Computer Programming", 190.54, "Books", 9, "Donald E. Knuth");
 		  	inventory[2]= new Product("Men's Business Cascual Shirt", 57.10, "Apparel", 2, 'M', "Teal");
 		  	inventory[3]= new Product("Organic Tumeric Soap", 11.25, "Bath & Beauty", 1);
-		  	inventory[4]= new Product("Head First Design Pattern", 37.35, "Books", 3, "Eric Freeman");
-		//Declaring local variables 
+		  	inventory[4]= new Product("Head First Design Pattern", 37.35, "Books", 3, "Eric Freeman");*/
+	
+		  /*//Declaring local variables 
 		  double totalAmount = 0.0;
 		  int userInput = 9;
-		  
+		  */
 		 //Creating Scanner object 
 		Scanner keyboard = new Scanner(System.in);
 		
 		//Menu option inside while(true) loop
 		while(true) {
-			System.out.println("Enter prooduct index (0 to 4) you want to buy. To exit the shopping cart, enter any number other than 0 to\r\n" + 
+			System.out.println("Enter product index (0 to 4) you want to buy. To exit the shopping cart, enter any number other than 0 to\r\n" + 
 					"4"); 
 			
 			//use for loop to loop through inventory array output = index : inventory[index] and it's name 
@@ -42,15 +60,25 @@ public class Shop {
 			//Try Scanner input 
 			try {
 			userInput = keyboard.nextInt();
+			if(inventory[userInput].buy()) {
+				System.out.println("Your purchase was successful!");
+					totalAmount += inventory[userInput].getPrice(); 
+				} else {
+				System.out.println("Sorry! This product is out of stock.");
+				}
 			}
 				//If user doe snot enter integer JVM throws exception 
 				catch(InputMismatchException e){
 				System.out.println("Invalid input, exiting the shopping cart! Your total amount is: " + totalAmount);
-				break;
+					break;
+				}
+				catch(ArrayIndexOutOfBoundsException e) {
+				System.out.println("Thank you for shopping with us! The total amount is " + totalAmount);
+					break;
 				}
 			
 			//if user enters a valid input 
-			if(userInput >= 0 && userInput <= 4) {
+			/*if(userInput >= 0 && userInput <= 4) {
 				if(inventory[userInput].buy()) {
 				System.out.println("Your purchase was successful!");
 					totalAmount += inventory[userInput].getPrice(); 
@@ -58,10 +86,10 @@ public class Shop {
 				System.out.println("Sorry! This product is out of stock.");
 				}
 			//entered other number other than 0,1,2,3,4
-			} else {
-				System.out.println("Thank you for shopping with us! The total amount is " + totalAmount);
-				break;
-			}
+			} else {*/
+			
+				
+			
 		}
 		keyboard.close(); 
 		
@@ -70,5 +98,6 @@ public class Shop {
 		for (Product products: inventory) {
 			System.out.println(products + "\n");
 		}*/
+		}
 	}
 }
